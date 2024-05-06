04413c591e6b210a99f2ce96120851b4f27ad43b
unknown <zenagsuma5@gmail.com>
2024-05-06 10:33:52 -0300

    Finalización de diasSemana, finalización del TP

---
+++ assets/diasSemana.js
@@ -0,0 +1,28 @@
+let diaSem = prompt("Ingrese un número del 1 al 7: ");
+
+switch (diaSem) {
+    case "1":
+    console.log("Número 1: Domingo");
+    break
+    case "2":
+    console.log("Número 2: Lunes");
+    break
+    case "3":
+    console.log("Número 3: Martes");
+    break
+    case "4":
+    console.log("Número 4: Miércoles");
+    break
+    case "5":
+    console.log("Número 5: Jueves");
+    break
+    case "6":
+    console.log("Número 6: Viernes");
+    break
+    case "7":
+    console.log("Número 7: Sábado");
+    break
+}
+
+if (diaSem <8 && diaSem >0);
+else console.log("Ingrese un numero valido");
